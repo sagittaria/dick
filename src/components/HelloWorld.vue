4953c64df7bb84af74c2fe0a58c9bb3306bb637c
Sean <shuyang_x@hotmail.com>
2019-11-07 11:58:05 +0800

    preset

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div style="display: flex;">
+    <div class="left" style="border: 1px solid red;width:50%;">
+      <video-player
+        class="video-player-box"
+        ref="videoPlayer"
+        :options="options"/>
+    </div>
+    <div class="right" style="border: 1px solid blue;width:10%;">
+      <el-input rows="6" type="textarea" v-model="text"></el-input>
+    </div>
   </div>
 </template>
 
 <script>
+import 'video.js/dist/video-js.css'
+import {videoPlayer} from 'vue-video-player'
+import 'videojs-flash'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {videoPlayer},
+  data() {
+    return {
+      text: '',
+      options: {
+        autoplay: true,
+        muted: true,
+        language: 'en',
+        playbackRates: [0.5, 1.0, 1.5, 2.0],
+        techOrder: ['html5', 'flash'],
+        sources: [
+          {type: 'rtmp/flv', src: 'rtmp://172.16.52.154:9911/live/1scr-sub'}
+        ]
+      },
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.video-player-box .video-js{
+  height: 500px;
+  width: 95%;
 }
 </style>
